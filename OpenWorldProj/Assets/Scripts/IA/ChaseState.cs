0a1949b9e6582c37db278fd73c5b1b9234170ff5
arthur-henrique <arthur.henrique.h@outlook.com>
2023-05-16 10:33:30 -0300

    damage controller

--- OpenWorldProj/Assets/Scripts/IA/ChaseState.cs
+++ OpenWorldProj/Assets/Scripts/IA/ChaseState.cs
@@ -8,7 +8,7 @@ public class ChaseState : StateMachineBehaviour
     NavMeshAgent agent;
     Transform player;
     public float chaseDistance;
-    public Enemy enemy;
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
