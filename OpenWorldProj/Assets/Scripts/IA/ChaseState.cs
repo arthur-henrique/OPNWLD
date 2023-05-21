289213574a23512db14642ae663361e557a8fcb7
kaykecelli <78507284+kaykecelli@users.noreply.github.com>
2023-05-21 20:00:05 -0300

    IA enemy

--- OpenWorldProj/Assets/Scripts/IA/ChaseState.cs
+++ OpenWorldProj/Assets/Scripts/IA/ChaseState.cs
@@ -8,11 +8,13 @@ public class ChaseState : StateMachineBehaviour
     NavMeshAgent agent;
     Transform player;
     public float chaseRange;
+    Animator anim;
     Vector3 currentPosition;
     
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        
         agent = animator.GetComponent<NavMeshAgent>();
         currentPosition = animator.GetComponent<Transform>().position;
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -23,11 +25,12 @@ public class ChaseState : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        anim = animator;
         agent.SetDestination(player.position);
         float distance = Vector3.Distance(player.position, animator.transform.position);
         if (distance < chaseRange)
         {
-            animator.SetBool("isChasing", false);
+            anim.SetBool("isChasing", false);
         }
         if (distance < 3.5)
         {
@@ -35,17 +38,15 @@ public class ChaseState : StateMachineBehaviour
         }
     }
 
-     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    
+
+
+   //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+   override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         agent.SetDestination(animator.transform.position);
     }
-    void OnDrawGizmos()
-    {
-        
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawSphere( currentPosition, chaseRange);
-    }
+    
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- OpenWorldProj/Assets/Scripts/IA/EnemyAI.cs
+++ OpenWorldProj/Assets/Scripts/IA/EnemyAI.cs
@@ -5,52 +5,143 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public enum States
+    {
+        idle,
+        patrol,
+        walk,
+        attack
+    }
+    [SerializeField]
+    private States states = States.idle;
+
+    //geral
+    public Animator animator;
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
     public Transform firePoint;
 
+    public float timeBetweenAttacks;
+    public GameObject projectile;
+
+
     //Patroling
     public Vector3 walkpoint;
     bool walkPointSet;
-    public float walkPointRange;
 
-    //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
-    public GameObject projectile;
 
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
-    private void Awake()
+    private bool alreadyAttacked;
+    public float walkPointRange;
+    float randomNumber; 
+
+    private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Idle());
+    }
+    private void StatesControl()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChangeState(States.walk);
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            ChangeState(States.attack);
+        }
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            
+            ChangeState(States.idle);
+        }
+
     }
     private void Update()
     {
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if (!playerInAttackRange && !playerInSightRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) Chasing();
-        if (playerInAttackRange && playerInSightRange) Attacking();
     }
-    private void Patroling()
+
+
+
+    public void ChangeState(States state)
     {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
+        states = state;
+        StopAllCoroutines(); // Interrompe a co-rotina atual (estado atual).
+        switch (states)
         {
-            agent.SetDestination(walkpoint);
+            case States.idle:
+                StartCoroutine(Idle());
+                break;
+               
+
+            case States.patrol:
+                StartCoroutine(Patroling());
+                break;
+
+
+
+            case States.walk:
+                StartCoroutine(Chase());
+                break;
+
+
+
+            case States.attack:
+                StartCoroutine(Attack());
+                break;
         }
+          
+    }
 
-        Vector3 distanceToWalkPoint = transform.position - walkpoint;
-        if(distanceToWalkPoint.magnitude < 1f)
-        {
+    IEnumerator Idle()
+    {
+
+        //animator.SetFloat("Velocidade", 0);
+            
+            yield return new WaitForEndOfFrame();
+            ChangeState(States.patrol);
+
+        
+    }
+
+    IEnumerator Patroling()
+    {
+        
+        
             walkPointSet = false;
-        }
 
+            if (!walkPointSet) SearchWalkPoint();
+
+            if (walkPointSet)
+            {
+                agent.SetDestination(walkpoint);
+            }
+
+            Vector3 distanceToWalkPoint = transform.position - walkpoint;
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                walkPointSet = false;
+            }
+            StatesControl();
+            
+            
+            yield return new WaitForSeconds(3f);
+            ChangeState(States.idle);
+             
+            
+            
+            
+
+        
     }
 
     private void SearchWalkPoint()
@@ -59,38 +150,76 @@ public class EnemyAI : MonoBehaviour
         float randomX = Random.Range(-walkPointRange, walkPointRange);
         walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-        if(Physics.Raycast(walkpoint, -transform.up, 2f, whatIsGround))
+        if (Physics.Raycast(walkpoint, -transform.up, 2f, whatIsGround))
         {
             walkPointSet = true;
         }
     }
-    private void Chasing()
-    {
-        agent.SetDestination(player.position);
-    }
-    private void Attacking()
-    {
 
-        
-        agent.SetDestination(transform.position);
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
+    IEnumerator Chase()
+    {
+        while (true)
         {
-            Rigidbody rb = Instantiate(projectile, firePoint.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            //animator.SetFloat("Velocidade", 1);
+            agent.SetDestination(player.position);
 
-            alreadyAttacked = true;
-            StartCoroutine(ResetAttack());
+            if (playerInAttackRange)
+            {
+                ChangeState(States.attack);
+            }
+
+            if (!playerInSightRange)
+            {
+                ChangeState(States.idle);
+            }
+
+            yield return new WaitForEndOfFrame();
+
         }
     }
-    IEnumerator ResetAttack()
+
+
+
+
+
+
+    IEnumerator Attack()
     {
-        yield return new WaitForSeconds(timeBetweenAttacks);
-        alreadyAttacked = false;
-        
+
+            
+         while (true)
+         {
+            agent.SetDestination(transform.position);
+            transform.LookAt(player);
+
+            if (!alreadyAttacked)
+            {
+                Rigidbody rb = Instantiate(projectile, firePoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+                rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+                rb.AddForce(transform.up * -1f, ForceMode.Impulse);
+
+                alreadyAttacked = true;
+                StartCoroutine(ResetAttack());
+            }
+
+
+            yield return new WaitForEndOfFrame();
+
+         }
+
+
     }
+   IEnumerator ResetAttack()
+   {
+     yield return new WaitForSeconds(timeBetweenAttacks);
+     alreadyAttacked = false;
+
+    if (!playerInAttackRange)
+    {
+         ChangeState(States.walk);
+    }
+
+   }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
@@ -99,5 +228,26 @@ public class EnemyAI : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, sightRange);
 
     }
-
 }
+
+
+       
+
+
+
+    
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
