dff53cbfd616e6b709b2ac5abe752af25573a6bb
Kayke Celli <Kaykecelli6@gmail.com>
2023-06-13 10:59:54 -0300

    Ajustes

--- OpenWorldProj/Assets/Scripts/Cristais/CristalCount.cs
+++ OpenWorldProj/Assets/Scripts/Cristais/CristalCount.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CristalCount : MonoBehaviour
 {
     public int quantidadeDeCristais;
-    public MovingPlatform movingPlatform;
+    public MovingPlatafomrPlayer movingPlatform;
 
     // Update is called once per frame
     void Update()
