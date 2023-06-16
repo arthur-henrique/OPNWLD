dc713d4df85fed1729197bba74bf1719806e1cfa
AmazingProgramadora <104537504+AmazingProgramadora@users.noreply.github.com>
2023-06-16 11:29:32 -0300

    Script do global volume atualizado

--- OpenWorldProj/Assets/Prefabs/Global Volume/BattleGlobalVolume.cs
+++ OpenWorldProj/Assets/Prefabs/Global Volume/BattleGlobalVolume.cs
@@ -1,61 +1,30 @@
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
-public class VignettePulse : MonoBehaviour
-{
-    PostProcessVolume m_Volume;
-    Vignette m_Vignette;
-    void Start()
-    {
-        // Create an instance of a vignette
-        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
-        m_Vignette.enabled.Override(true);
-        m_Vignette.intensity.Override(1f);
-
-        // Use the QuickVolume method to create a volume with a priority of 100, and assign the vignette to this volume
-        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
-
-        void Update()
-        {
-            // Change vignette intensity using a sinus curve
-            m_Vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
-        }
-
-        void OnDestroy()
-        {
-            RuntimeUtilities.DestroyVolume(m_Volume, true, true);
-        }
-    }
-}
-
-
-
-
-
-
-
-/*using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class VolumeTest : MonoBehaviour
+public class BattleGlobalVolume : MonoBehaviour
 {
 
     private Volume volumeVar;
     private Bloom bloomVar;
     private Vignette vignetteVar;
+    
     // Start is called before the first frame update
     void Start()
     {
         volumeVar = GetComponent<Volume>();
         volumeVar.profile.TryGet(out bloomVar);
         volumeVar.profile.TryGet(out vignetteVar);
+        vignetteVar.intensity = new ClampedFloatParameter(0.5f,0f, 1f,true);
     }
 
     [ContextMenu("test")]
     private void Test()
     {
         bloomVar.scatter.value = 0.1f;
-        vignetteVar.intensity.value = 0.5f;
+        vignetteVar.intensity.value = 0.5f; //tem que ter as opções já habilitadas anteriormente
     }
 
-}*/
+}
+
+//design pattern observer
