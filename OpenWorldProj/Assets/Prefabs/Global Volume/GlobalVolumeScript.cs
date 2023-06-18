dee39049b75cf6933196620c50ec6c77c07e50ad
Anninha <anninha0307trabalhos@gmail.com>
2023-06-18 16:12:32 -0300

    Alguns prefabs de Global Volume ajustados

--- OpenWorldProj/Assets/Prefabs/Global Volume/GlobalVolumeScript.cs
+++ OpenWorldProj/Assets/Prefabs/Global Volume/GlobalVolumeScript.cs
@@ -2,32 +2,43 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class BattleGlobalVolume : MonoBehaviour, IObserver, IDeathObserver
+public class GlobalVolumeScript : MonoBehaviour, IObserver, IDeathObserver
 {
 
     private Volume volumeVar;
-    private Bloom bloomVar;
+    //private Bloom bloomVar;
     private Vignette vignetteVar;
+    static float t = 0.0f;
 
     public HealthControl agentHealth;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         volumeVar = GetComponent<Volume>();
-        volumeVar.profile.TryGet(out bloomVar);
+        //volumeVar.profile.TryGet(out bloomVar);
         volumeVar.profile.TryGet(out vignetteVar);
-        vignetteVar.intensity = new ClampedFloatParameter(0.5f,0f, 1f,true);
+        //vignetteVar.intensity = new ClampedFloatParameter(0.5f,0f, 1f,true);
         AddObserver();
     }
 
-    [ContextMenu("test")]
+    void Update()
+    {
+        vignetteVar.intensity.value = Mathf.Lerp(0, 0.455f, t);
+        
+        if (t < 1.0f)
+        {
+        t += 2.3f * Time.deltaTime;
+        }
+        //vignetteVar.intensity.value = 0.455f;
+
+    }
+
+    /*[ContextMenu("test")]
     private void Test()
     {
         bloomVar.scatter.value = 0.1f;
         vignetteVar.intensity.value = 0.5f; //tem que ter as opções já habilitadas anteriormente
-    }
+    }*/
 
     void AddObserver()
     {
@@ -43,7 +54,7 @@ public class BattleGlobalVolume : MonoBehaviour, IObserver, IDeathObserver
 
     public void OnNotifyDamage(float damage)
     {
-
+        print("levou dano");
     }
 
     public void OnNotifyHeal(float heal)
